20f29d0b9539c55c1b357e69134d0a92b0dc0a5a
Ailton <ailton.msj@gmail.com>
2019-02-24 13:27:08 -0300

    Ajustment to validade the token

--- src/main/java/br/com/amsj/spring/oauth/config/OAuth2Config.java
+++ src/main/java/br/com/amsj/spring/oauth/config/OAuth2Config.java
@@ -1,13 +1,16 @@
 package br.com.amsj.spring.oauth.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
 
 @Configuration
 @EnableResourceServer
@@ -27,4 +30,15 @@ public class OAuth2Config extends ResourceServerConfigurerAdapter{
 			.withUser("clark").password(encoder.encode("password")).roles("USER").and().
 			withUser("bruce").password(encoder.encode("password")).roles("USER");
 	}
+    
+    @Primary
+    @Bean
+    public RemoteTokenServices tokenService() {
+    	
+        RemoteTokenServices tokenService = new RemoteTokenServices();
+        tokenService.setCheckTokenEndpointUrl("http://localhost:9091/oauth/check_token");
+        tokenService.setClientId("carServiceClient");
+        tokenService.setClientSecret("clientSecret");
+        return tokenService;
+    }
 }
